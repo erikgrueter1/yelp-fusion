f81ac05cc2fb921d970534353bf9fe4a70545b66
Erik Grueter <egrueter@jobcase.com>
2018-05-27 21:01:41 -0400

    initial commit

---
+++ lib/yelp/fusion.rb
@@ -0,0 +1,7 @@
+require "yelp/fusion/version"
+
+module Yelp
+  module Fusion
+    # Your code goes here...
+  end
+end
---
+++ lib/yelp/fusion/version.rb
@@ -0,0 +1,5 @@
+module Yelp
+  module Fusion
+    VERSION = "0.1.0"
+  end
+end
