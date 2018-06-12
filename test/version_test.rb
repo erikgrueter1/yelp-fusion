a1fe60cbaf8caec50ef2c512f9a762828655a9ab
Lyra <lkatzman@jobcase.com>
2018-06-12 16:49:54 -0400

    update version tests

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 require 'pry-coolline'
 
 class VersionTest < Minitest::Test
-
   def test_the_yelp_version_can_be_obtained
     refute_empty Yelp::Fusion::VERSION
     assert_equal true, Yelp::Fusion::VERSION.is_a?(String)
